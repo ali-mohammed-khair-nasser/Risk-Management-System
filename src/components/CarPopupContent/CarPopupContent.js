b7e8bd05e4ec16bfa70540666c8a1723a8df350b
Ali Nasser <ali.nasser.it@gmail.com>
2021-08-17 19:13:52 +0300

    hotfix

--- src/components/CarPopupContent/CarPopupContent.js
+++ src/components/CarPopupContent/CarPopupContent.js
@@ -12,10 +12,9 @@ const CarPopupContent = ( props ) => (
                     <Col md="4" className="progress-bar"><RoundedProgressBar progress={ 64 } time={ props.neddedTime } distance={ props.neededDistance } /></Col>
                     <Col className="car-info">
                         <h5 className="title">Vehicle information</h5>
-                        { console.log(props.elementInfo) }
                         <p className="meta-info"><span><span className="circle"></span> Status:</span> { props.elementInfo.state }</p>
                         <p className="meta-info"><span><span className="circle"></span> Name:</span> { props.elementInfo.name }</p>
-                        {/* <p className="meta-info"><span><span className="circle"></span> Center:</span> { props.elementInfo.station.name }</p> */}
+                        <p className="meta-info"><span><span className="circle"></span> Center:</span> { props.elementInfo.station.name }</p>
                     </Col>
                 </Row>
             </Container>
